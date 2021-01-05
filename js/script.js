ecdb5647b830efedcb62b5c2da5323f8049754c8
Glitch (immense-horse-course) <none>
2021-01-05 23:54:12 +0000

    ⛈🕷 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+let computerChoice = "";
+
 //GLOBAL VARIABLES
 /* global $ */
 
@@ -12,4 +14,26 @@ $("#shoot").click(function() {
 
 $("#shoot").click(function() { 
   $("#computerChoice").text();
+});
+
+
+
+$("#shoot").click(function() {
+    let computer = Math.random();
+    console.log(computer);
+    if (computer > 0.666) {
+      computerChoice = "paper";
+        $(".text").text("paper");
+                                               
+    } else if ( computer > 0.333){ 
+      computerChoice = "scissors";
+      $(".text").text("scissors"); 
+                                                                                                     
+    } else {
+      computerChoice = "rock";
+        $(".text").text("rock");
+    }
+	
+  $("#computerChoice").html(computerChoice);
+  
 });
